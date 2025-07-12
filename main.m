2da510f70764b5de4a9f506b00c0d3efd49bf2e2
liu227857 <lii227857@gmail.com>
2025-07-12 14:15:19 +0800

    update mat files

--- main.m
+++ main.m
@@ -1,21 +1,20 @@
 %% Disk-Shaped Area-Preserving Parameterization
 %
 % Main program:
-% [uv, Iter, VB, VI] = DiskAEM(F, V, Options)
+% [uv, VB, VI] = DiskAEM(F, V)
 % 
 % Required Input:
 % F: #F x 3 triangulations of an open triangle mesh
 % V: #V x 3 vertex coordinates of an open triangle mesh
 %
-% Optional Input:
-% Options.MaxIter: the maximum iterative number (default: 200)
-% Options.Tol: the tolerance of stoping criteria (default: 1e-6)
-%
 % Output:
 % uv: #V x 2 vertex coordinates of the disk-shaped area-preserving map
-% Iter: number of iterations
-% VI: indices of interior vertices
 % VB: indices of boundary vertices
+% VI: indices of interior vertices
+%
+% Optional Input:
+% DiskAEM( __, "MaxIter", Value): the maximum iterative number (default: 200)
+% DiskAEM( __, "Tol", Value): the tolerance of stoping criteria (default: 1e-6)
 % 
 % Remark:
 % If you use this code in your own work, please cite the following paper:
@@ -23,6 +22,9 @@
 %     for Disk Area-Preserving Parameterizations"
 % doi: 10.1007/s10915-024-02594-2
 %
+% License:
+% This software is released for academic and research purposes only.
+% Commercial use is not permitted without prior written permission from the authors.
 % Copyright (c), Shu-Yung Liu and Mei-Heng Yueh
 
 
